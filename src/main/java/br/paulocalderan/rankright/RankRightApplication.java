89092d032e33218e4750f3f3180d1fc07c8581ed
Paulo Calderan <paulo_calderan@outlook.com>
2024-01-16 07:53:01 -0300

    inicio projeto

---
+++ src/main/java/br/paulocalderan/rankright/RankRightApplication.java
@@ -0,0 +1,13 @@
+package br.paulocalderan.rankright;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RankRightApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RankRightApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/paulocalderan/rankright/RankRightApplicationTests.java
@@ -0,0 +1,13 @@
+package br.paulocalderan.rankright;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RankRightApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
